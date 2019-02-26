8651d57c678fe37612ece0056ebfb57ce83036d6
sangramdesai123 <sangram.desai@somaiya.edu>
2019-02-26 18:04:36 +0530

    login page

--- js/chat.js
+++ js/chat.js
@@ -12,4 +12,9 @@ function chat(val) {
 	var div='<div class="card question"><img class="card-img-top" src="img/l1.jpg" style="width:380px;height:200px;border-radius: 20px;"><div class="card-body"><h4>John Doe</h4><p class="card-text">Some example text some example text. John Doe is an architect and engineer</p></div></div>'
 	var question = div;        // Create text with HTML
 	$("#homecard").append(question);   // Append new elements
+  var elem = document.getElementById('homecard');
+  elem.scrollTop = elem.scrollHeight;
 }
+window.setInterval(function() {
+
+}, 1000);
